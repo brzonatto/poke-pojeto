1de42431ee9c0092c1e2d5ae7e01c54394788bc7
Leo-kardo <leonardo_c_g@hotmail.com>
2021-11-05 16:02:53 -0300

    métodos get, put e list do pokémon

--- src/main/java/com/dbc/controller/PokemonController.java
+++ src/main/java/com/dbc/controller/PokemonController.java
@@ -2,16 +2,16 @@ package com.dbc.controller;
 
 import com.dbc.dto.PokemonCreateDTO;
 import com.dbc.dto.PokemonDTO;
+import com.dbc.exceptions.RegraDeNegocioException;
 import com.dbc.service.PokemonService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
@@ -23,12 +23,28 @@ public class PokemonController {
 
     private final PokemonService pokemonService;
 
-    @ApiOperation("200")
+    @ApiOperation("Adicionando Pokémon")
     @PostMapping
     public PokemonDTO create (@RequestBody PokemonCreateDTO pokemonCreateDTO){
         PokemonDTO pokemonCriado = pokemonService.create(pokemonCreateDTO);
         return pokemonCriado;
     }
+
+    @ApiOperation("Listando Pokémon")
+    @GetMapping
+    public List<PokemonDTO> list(){return pokemonService.list();}
+
+
+    @ApiOperation("Editando Pokémon")
+    @PutMapping("/{idPokemon}")
+    public PokemonDTO update(@PathVariable ("idPokemon") Integer id,
+                             @Valid @RequestBody PokemonCreateDTO pokemonCreateDTO) throws RegraDeNegocioException{
+        PokemonDTO pokemonEditado = pokemonService.update(id, pokemonCreateDTO);
+        return pokemonEditado;
+    }
+
+
+
 
 
 
--- src/main/java/com/dbc/service/PokemonService.java
+++ src/main/java/com/dbc/service/PokemonService.java
@@ -3,10 +3,14 @@ package com.dbc.service;
 import com.dbc.dto.PokemonCreateDTO;
 import com.dbc.dto.PokemonDTO;
 import com.dbc.entity.PokemonEntity;
+import com.dbc.exceptions.RegraDeNegocioException;
 import com.dbc.repository.PokemonRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +23,18 @@ public class PokemonService {
         PokemonEntity pokemonEntity = objectMapper.convertValue(pokemonCreateDTO, PokemonEntity.class);
         PokemonEntity pokemonCriado = pokemonRepository.create(pokemonEntity);
         return objectMapper.convertValue(pokemonCriado, PokemonDTO.class);
+    }
+
+    public List<PokemonDTO> list() {
+        return pokemonRepository.list().stream()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PokemonDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public PokemonDTO update(Integer id, PokemonCreateDTO pokemonCreateDTO) throws RegraDeNegocioException {
+        PokemonEntity pokemonEntity = objectMapper.convertValue(pokemonCreateDTO, PokemonEntity.class);
+        PokemonEntity pokemonAtualizado = pokemonRepository.update(id, pokemonEntity);
+        PokemonDTO pokemonDTO = objectMapper.convertValue(pokemonAtualizado, PokemonDTO.class);
+        return objectMapper.convertValue(pokemonAtualizado, PokemonDTO.class);
     }
 }
