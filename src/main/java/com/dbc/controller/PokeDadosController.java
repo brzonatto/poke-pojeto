19a9b433ea8177f3ef849bbc814553c8328032fd
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-11-07 22:17:01 -0300

    INIT POKEDADOS

---
+++ src/main/java/com/dbc/controller/PokeDadosController.java
@@ -0,0 +1,16 @@
+package com.dbc.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+@RequestMapping("/pokemon/dados")
+public class PokeDadosController {
+
+}
