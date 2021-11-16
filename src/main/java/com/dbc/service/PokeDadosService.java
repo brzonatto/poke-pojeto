484f859a15e05ffda9e247dd48b0a392503a9137
Marcelo <marcelo.ungaretti@gmail.com>
2021-11-16 11:01:29 -0300

    Novo método pokeDados

--- src/main/java/com/dbc/service/PokeDadosService.java
+++ src/main/java/com/dbc/service/PokeDadosService.java
@@ -2,6 +2,7 @@ package com.dbc.service;
 
 import com.dbc.dto.*;
 import com.dbc.entity.PokemonEntity;
+import com.dbc.exceptions.RegraDeNegocioException;
 import com.dbc.repository.EvolucaoRepository;
 import com.dbc.repository.HabilidadeRepository;
 import com.dbc.repository.PokemonRepository;
@@ -10,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,38 +24,73 @@ public class PokeDadosService {
     private final EvolucaoRepository evolucaoRepository;
     private final ObjectMapper objectMapper;
 
-    public List<PokeDadosDTO> list() {
-        return pokemonRepository.findAll()
-                .stream()
-                .map(pokemon -> {
-                    PokemonCreateDTO pokemonCreateDTO = objectMapper.convertValue(pokemon, PokemonCreateDTO.class);
-                    PokeDadosDTO pokeDadosDTO = new PokeDadosDTO();
-                    pokeDadosDTO.setPokemon(pokemonCreateDTO);
-                    pokeDadosDTO.setTipos(
-                            pokemon.getTipos()
-                                    .stream()
-                                    .map(tipo -> objectMapper.convertValue(tipo, TipoPokemonCreateDTO.class))
-                                    .collect(Collectors.toList())
-                    );
-                    pokeDadosDTO.setHabilidades(
-                            pokemon.getHabilidades()
-                                    .stream()
-                                    .map(habilidade -> objectMapper.convertValue(habilidade, HabilidadeCreateDTO.class))
-                                    .collect(Collectors.toList())
-                    );
+    private final PokemonService pokemonService;
 
-                    if (pokemon.getEvolucaoEntity() != null) {
-                        EvolucaoNomesDTO evolucaoNomesDTO = new EvolucaoNomesDTO();
-                        evolucaoNomesDTO.setEstagioUm(pokemon.getEvolucaoEntity().getEstagioUm().getNome());
-                        evolucaoNomesDTO.setEstagioDois(pokemon.getEvolucaoEntity().getEstagioDois().getNome());
-                        if (pokemon.getEvolucaoEntity().getEstagioTres() != null) {
-                            evolucaoNomesDTO.setEstagioTres(pokemon.getEvolucaoEntity().getEstagioTres().getNome());
+    public List<PokeDadosDTO> list(Integer idPoke) throws RegraDeNegocioException {
+        List<PokeDadosDTO> listaPokemonComTodosDados = new ArrayList<>();
+
+        if (idPoke == null) {
+
+            return  pokemonRepository.findAll()
+                    .stream()
+                    .map(pokemon -> {
+                        PokemonCreateDTO pokemonCreateDTO = objectMapper.convertValue(pokemon, PokemonCreateDTO.class);
+                        PokeDadosDTO pokeDadosDTO = new PokeDadosDTO();
+                        pokeDadosDTO.setPokemon(pokemonCreateDTO);
+                        pokeDadosDTO.setTipos(
+                                pokemon.getTipos()
+                                        .stream()
+                                        .map(tipo -> objectMapper.convertValue(tipo, TipoPokemonCreateDTO.class))
+                                        .collect(Collectors.toList())
+                        );
+                        pokeDadosDTO.setHabilidades(
+                                pokemon.getHabilidades()
+                                        .stream()
+                                        .map(habilidade -> objectMapper.convertValue(habilidade, HabilidadeCreateDTO.class))
+                                        .collect(Collectors.toList())
+                        );
+
+                        if (pokemon.getEvolucaoEntity() != null) {
+                            EvolucaoNomesDTO evolucaoNomesDTO = new EvolucaoNomesDTO();
+                            evolucaoNomesDTO.setEstagioUm(pokemon.getEvolucaoEntity().getEstagioUm().getNome());
+                            evolucaoNomesDTO.setEstagioDois(pokemon.getEvolucaoEntity().getEstagioDois().getNome());
+                            if (pokemon.getEvolucaoEntity().getEstagioTres() != null) {
+                                evolucaoNomesDTO.setEstagioTres(pokemon.getEvolucaoEntity().getEstagioTres().getNome());
+                            }
+                            pokeDadosDTO.setEvolucao(evolucaoNomesDTO);
                         }
-                        pokeDadosDTO.setEvolucao(evolucaoNomesDTO);
-                    }
 
-                    return pokeDadosDTO;
-                })
-                .collect(Collectors.toList());
+                        return pokeDadosDTO;
+                    })
+                    .collect(Collectors.toList());
+        }
+        PokemonEntity pokemonEntity = pokemonService.findById(idPoke);
+        PokeDadosDTO pokeDadosDTO = objectMapper.convertValue(pokemonEntity, PokeDadosDTO.class);
+        pokeDadosDTO.setPokemon(objectMapper.convertValue(pokemonEntity, PokemonCreateDTO.class));
+        pokeDadosDTO.setTipos(
+                pokemonEntity.getTipos()
+                        .stream()
+                        .map(tipo -> objectMapper.convertValue(tipo, TipoPokemonCreateDTO.class))
+                        .collect(Collectors.toList())
+        );
+        pokeDadosDTO.setHabilidades(
+                pokemonEntity.getHabilidades()
+                        .stream()
+                        .map(habilidade -> objectMapper.convertValue(habilidade, HabilidadeCreateDTO.class))
+                        .collect(Collectors.toList())
+        );
+        if (pokemonEntity.getEvolucaoEntity() != null) {
+            EvolucaoNomesDTO evoNomesDTO = new EvolucaoNomesDTO();
+            evoNomesDTO.setEstagioUm(pokemonEntity.getEvolucaoEntity().getEstagioUm().getNome());
+            evoNomesDTO.setEstagioDois(pokemonEntity.getEvolucaoEntity().getEstagioDois().getNome());
+            if (pokemonEntity.getEvolucaoEntity().getEstagioTres() != null) {
+                evoNomesDTO.setEstagioTres(pokemonEntity.getEvolucaoEntity().getEstagioTres().getNome());
+            }
+            pokeDadosDTO.setEvolucao(evoNomesDTO);
+        }
+
+        listaPokemonComTodosDados.add(pokeDadosDTO);
+
+        return listaPokemonComTodosDados;
     }
 }
