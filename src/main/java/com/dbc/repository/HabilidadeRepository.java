7c3b38c34dd4865cd9d79bc0da8a26c9e91aa589
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-11-19 14:10:28 -0300

    ajustes Security

--- src/main/java/com/dbc/repository/HabilidadeRepository.java
+++ src/main/java/com/dbc/repository/HabilidadeRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-
 @Repository
 public interface HabilidadeRepository extends JpaRepository<HabilidadeEntity, Integer> {
     @Modifying
