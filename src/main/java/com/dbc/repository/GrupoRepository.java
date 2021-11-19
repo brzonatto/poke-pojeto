fa6308685b8cba9b0a5a74da734bc464eb4079b1
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-11-19 14:16:26 -0300

    mais ajustes

--- src/main/java/com/dbc/repository/GrupoRepository.java
+++ src/main/java/com/dbc/repository/GrupoRepository.java
@@ -1,6 +1,6 @@
 package com.dbc.repository;
 
-import com.dbc.pessoaapi.entity.GrupoEntity;
+import com.dbc.entity.GrupoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
